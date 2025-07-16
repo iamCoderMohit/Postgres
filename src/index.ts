b0d21b1ca8edb739323cd984aaa5298421bf7d02
Mohit Joshi <mohitjoshiu@gmail.com>
2025-07-16 19:14:02 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Client } from "pg";
 
 const client = new Client({
-    connectionString: "postgresql://neondb_owner:npg_YmrUjv8PLih5@ep-lucky-grass-a8syd38q-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require"
+    connectionString: ""
 })
 
 
